688e3acf4dfc19e87bb79fbee73acebd039e6096
Tạ Tuấn Anh <tripleseven190504@gmail.com>
2023-12-20 07:26:29 +0700

    update excel

--- excel_test.py
+++ excel_test.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
 
 df = pd.read_excel('du_lieu/data.xlsx')
-customer_code = df.iloc[0:, 0].values.tolist()
+customer_code = df.iloc[:, 0].dropna().values.tolist()
+print(customer_code)
